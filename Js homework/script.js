345075ba11a22679fc06f4a6a39c248f897e7e3b
Gmoseshvili <gmoseshvili@europebet.com>
2023-10-17 23:03:22 +0400

    hmwrk
    my homework

---
+++ Js homework/script.js
@@ -0,0 +1,94 @@
+// document.write("3.1 ")
+// document.write("2X-3Y+Z=0 ")
+// var y=Number(prompt("input Y"))
+// var z=Number(prompt("input z"))
+// x=(3*y-z)/2
+// document.write("x=",x)
+
+// document.write("3.2")
+// var a=Number(prompt("input a "))
+// var b=Number(prompt("input b "))
+// var c=Math.sqrt(a*a+b*b)
+// document.write(c)
+
+// document.write("3.3")
+// var a=Number(prompt("input a "))
+// var b=Number(prompt("input b "))
+// var c=Number(prompt("input c "))
+// var p=a+b+b
+// document.write("p=",p)
+
+// document.write("3.4")
+// document.write("2x^2-2x+13=0")
+// var d=Math.sqrt(4+13*2*4)
+// var x1=(2+d)/2
+// var x2=(2-d)/2
+// document.write(x1,x2)
+
+// document.write("2.1")
+// var name=prompt("input your name")
+// var surname=prompt("input your surname")
+// var age=Number(prompt("input your age "))
+// if(age>=18){
+//     document.write("Hello",`${name},${surname}`)
+// }
+// else{
+//     document.ownerDocument("you are not 18!!")
+// }
+
+// document.write("2.2 ")
+// var q1=Number(prompt("რომელ წელს დაეცა კონსტანტინეპოლი? "))
+// var score=0;
+// if(q1==1453){
+//     score++;
+// }
+// var q2=Number(prompt("რამდენი გვერდი აქვს სამკუთხედს? "))
+// if(q2==3){
+//     score++;
+// }
+// var q3=Number(prompt("რამდენი გვერდი აქვს ოთხკუთხედს? "))
+// if(q3==4){
+//     score++;
+// }
+// document.write("your score",score)
+
+// document.write("2.3")
+// var a=Number(prompt("input first number "))
+// var b=Number(prompt("input second number "))
+// var operation=prompt("input operation")
+// switch(operation){
+//     case "+":
+//         document.write(a+b);
+//         break;
+//     case "-":
+//         document.write(a+b);
+//         break;
+//     case "*":
+//         document.write(a*b);
+//         break;
+//     case "/":
+//         document.write(a/b);
+//         break;
+//     default:
+//         document.write("incorrect symbol")
+// }
+
+// document.write("1.1")
+// var name=prompt("input your name")
+// var surname=prompt("input your surname")
+// var age=Number(prompt("input your age"))
+// document.write(name,surname,age)
+
+// document.write("1.2")
+// var name=prompt("input your name")
+// var surname=prompt("input your surname")
+// var age=Number(prompt("input your age"))
+// var mail=prompt("input your mail")
+// var gender=prompt("input your gender")
+// var text=name+surname+age+mail+gender
+// document.write(text)
+
+// document.write("1.3")
+// var fullName=prompt("input your full name")
+// var h1=`<h1>${fullName}</h1>`
+// document.write(h1)
